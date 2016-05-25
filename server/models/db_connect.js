cac43b795330e8f4e0c11eadb326053cb833feb8
Matthew Keymer <mastercyrax@hotmail.com>
2016-05-25 12:15:35 -0500

    Fixed database name

--- server/models/db_connect.js
+++ server/models/db_connect.js
@@ -1,6 +1,6 @@
 var mongoose = require("mongoose");
 
-var mongoURI = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost/my_villain_db';
+var mongoURI = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost/manga';
 var MongoDB = mongoose.connect(mongoURI).connection;
 
 MongoDB.on("error", function(err) {
